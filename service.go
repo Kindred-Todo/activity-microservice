43beadf8bfabb43aa731f7e04a0ca941793b2d9b
Abhik Ray <abhikaboy@gmail.com>
2025-08-16 20:33:45 -0400

    tasks complete

--- service.go
+++ service.go
@@ -80,8 +80,12 @@ func updateStreak(ctx context.Context, userId primitive.ObjectID, completedAt ti
 			"$match": bson.M{"_id": userId},
 		},
 		{
+			"$inc": bson.M{
+				"tasks_complete": 1,
+			},
+		},
+		{
 			"$addFields": bson.M{
-				"tasks_complete": bson.M{"$inc": 1},
 				"streak": bson.M{
 					"$cond": bson.M{
 						"if":   bson.M{"$eq": []interface{}{"$streakEligible", true}},
